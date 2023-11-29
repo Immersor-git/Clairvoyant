f40db3b0f5fc6e844015facabed3b3079d5fb58e
Immersor-git <matt.halderman11@gmail.com>
2023-11-28 16:38:28 -0800

    committin

--- dbdemo.js
+++ dbdemo.js
@@ -1,14 +1,12 @@
+import { doc, setDoc } from "firebase/firestore"; 
 
-import { getDatabase, ref, set } from "firebase/database";
-const database = getDatabase();
-
-function writeUserData(userId, name, email, imageUrl) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
+function writeItemName(name) {
+    await setDoc(doc(db, "cities", "LA"), {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+      });
+      
 }
 
 function displayList() {
